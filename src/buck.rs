a23d8a57769e777a7a741264739f298e63b4d755
Tobias Breitwieser <tobias@breitwieser.biz>
2024-05-06 17:21:27 +0200

    start BUCK config

---
+++ src/buck.rs
@@ -0,0 +1,106 @@
+
+
+#[derive(PartialEq, Debug)]
+struct Voltage(u8);
+
+impl Voltage {
+    pub fn from_millivolts(millivolts: u16) -> Self {
+        if millivolts < 1100 {
+            return Voltage(0u8)
+        } else if millivolts > 3300 {
+            return Voltage(24u8)
+        }
+        return Voltage((millivolts / 100 - 10) as u8);
+    }
+
+    pub fn to_millivolts(&self) -> u16 {
+        if self.0 == 0 {
+            return 0;
+        } else if self.0 == 24 {
+            return 3300;
+        }
+        return (self.0 as u16 + 10) * 100;
+    }
+}
+
+pub enum ModeControl {
+    Auto,
+    PWM,
+    PFM,
+    GPIO0,
+    GPIO1,
+    GPIO2,
+    GPIO3,
+    GPIO4,
+}
+
+pub enum OnOffControl {
+    Software,
+    Vset,
+    GPIO0,
+    GPIO1,
+    GPIO2,
+    GPIO3,
+    GPIO4,
+}
+
+pub enum RetentionControl {
+    Off,
+    GPIO0,
+    GPIO1,
+    GPIO2,
+    GPIO3,
+    GPIO4,
+}
+
+struct BuckConfig {
+    pub enabled: bool,
+    pub is_software_set: bool,
+    pub auto_cap_discharge: bool,
+    pub output_voltage: Voltage,
+    pub retention_voltage: Voltage,
+    pub retention_control: RetentionControl,
+    pub on_off_control: OnOffControl,
+    pub mode_control: ModeControl,
+}
+
+impl Default for BuckConfig {
+    fn default() -> Self {
+        BuckConfig {
+            enabled: false,
+            is_software_set: false,
+            auto_cap_discharge: false,
+            output_voltage: Voltage(0),
+            retention_voltage: Voltage(0),
+            retention_control: RetentionControl::Off,
+            on_off_control: OnOffControl::Software,
+            mode_control: ModeControl::Auto,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_from_millivolts() {
+        assert_eq!(Voltage::from_millivolts(0), Voltage(0));
+        assert_eq!(Voltage::from_millivolts(100), Voltage(0));
+        assert_eq!(Voltage::from_millivolts(1000), Voltage(0));
+        assert_eq!(Voltage::from_millivolts(1100), Voltage(1));
+        assert_eq!(Voltage::from_millivolts(3000), Voltage(20));
+        assert_eq!(Voltage::from_millivolts(3300), Voltage(23));
+        assert_eq!(Voltage::from_millivolts(3400), Voltage(24));
+        assert_eq!(Voltage::from_millivolts(25500), Voltage(24));
+        assert_eq!(Voltage::from_millivolts(25600), Voltage(24));
+    }
+
+    #[test]
+    fn test_to_millivolts() {
+        assert_eq!(Voltage(0).to_millivolts(), 0);
+        assert_eq!(Voltage(1).to_millivolts(), 1100);
+        assert_eq!(Voltage(20).to_millivolts(), 3000);
+        assert_eq!(Voltage(23).to_millivolts(), 3300);
+        assert_eq!(Voltage(24).to_millivolts(), 3300);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,104 @@
 #![no_std]
 
+use embedded_hal_async::digital::Wait;
+use embedded_hal_async::i2c::Error as I2CHALError;
+use embedded_hal_async::i2c::ErrorKind;
+
 mod fmt;
 mod registers;
+mod interrupt;
+mod sysreg;
+mod charger;
+mod buck;
+mod ldo;
+mod leddrv;
+mod gpio;
+mod sysmon;
+mod pof;
+mod timer;
+mod ship_hibernation;
+mod errlog;
 
-const NRF1300_SLAVE_ADDRESS: u8 = 0b110_1011;
+const NRF1300_SLAVE_ADDRESS: u8 = 0b0110_1011;
+
+enum Error {
+    I2CError(ErrorKind),
+    NoPinAssigned
+}
+#[non_exhaustive]
+enum InterruptSource{
+    ADC,
+
+}
+
+struct NRF1300<I2C, SHPHLD, INTERRUPT>
+where
+    I2C: embedded_hal_async::i2c::I2c,
+    SHPHLD: embedded_hal::digital::OutputPin,
+    INTERRUPT: embedded_hal::digital::InputPin + embedded_hal_async::digital::Wait,
+{
+    i2c: I2C,
+    shphld: Option<SHPHLD>,
+    interrupt: Option<INTERRUPT>,
+}
+
+impl<I2C, SHPHLD, INTERRUPT>
+    NRF1300<I2C, SHPHLD, INTERRUPT>
+where
+    I2C: embedded_hal_async::i2c::I2c,
+    SHPHLD: embedded_hal::digital::OutputPin,
+    INTERRUPT: embedded_hal::digital::InputPin + embedded_hal_async::digital::Wait,
+{
+    pub async fn new(
+        i2c: I2C,
+        shphld: Option<SHPHLD>,
+        interrupt: Option<INTERRUPT>,
+    ) -> Self {
+        let mut nrf1300 = NRF1300 {
+            i2c,
+            shphld,
+            interrupt,
+        };
+
+        nrf1300
+    }
+
+    async fn write_register(&mut self, register: u16, value: u8) -> Result<(), Error> {
+        let reg_addr = register.to_be_bytes();
+        let data: [u8; 3]  = [reg_addr[0], reg_addr[1], value];
+
+        match self.i2c
+            .write(NRF1300_SLAVE_ADDRESS, &data)
+            .await {
+            Ok(_) => Ok(()),
+            Err(e) => Err(Error::I2CError(e.kind())),
+        }
+    }
+
+    async fn read_register(&mut self, register: u16) -> Result<u8, Error> {
+        let mut data = [0];
+        match self.i2c
+            .write_read(NRF1300_SLAVE_ADDRESS, &register.to_be_bytes(), &mut data)
+            .await {
+            Ok(_) => Ok(data[0]),
+            Err(e) => Err(Error::I2CError(e.kind())),
+        }
+    }
+
+    pub async fn wait_for_interrupt(&mut self) -> Result<InterruptSource, Error> {
+        match self.interrupt {
+            Some(ref mut interrupt) => {
+                let _ = interrupt.wait_for_high().await;
+                self.get_interrupt_source().await
+            },
+            None => Err(Error::NoPinAssigned),
+        }
+    }
+
+    async fn get_interrupt_source(&mut self) -> Result<InterruptSource, Error> {
+        todo!("Implement this function")
+    }
+
+
+
+}
